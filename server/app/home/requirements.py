0cbdf1109a046c22ac4c5275bc0e3ae39d6b833e
victor barbier <victor.barbier@recherche.gouv.fr>
2025-03-28 10:34:33 +0100

    feat(home): show versions

---
+++ server/app/home/requirements.py
@@ -0,0 +1,22 @@
+import importlib.metadata as metadata
+import sys
+
+
+def read_requirements(file_path="requirements.txt"):
+    with open(file_path, "r") as file:
+        requirements = file.readlines()
+    return [req.strip() for req in requirements]
+
+
+def get_installed_versions():
+    requirements = read_requirements()
+    installed_versions = {}
+    installed_versions["Python"] = sys.version
+    for req in requirements:
+        package_name = req.split("==")[0]
+        try:
+            version = metadata.version(package_name)
+            installed_versions[package_name] = version
+        except metadata.PackageNotFoundError:
+            installed_versions[package_name] = None
+    return installed_versions
--- server/app/home/routes.py
+++ server/app/home/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.home.requirements import get_installed_versions
 
 home_routes = Blueprint(
     "home",
@@ -9,3 +10,8 @@ home_routes = Blueprint(
 @home_routes.route("/", methods=["GET"])
 def home():
     return "LLM Fine-Tuning Application"
+
+
+@home_routes.route("/versions", methods=["GET"])
+def versions():
+    return jsonify(get_installed_versions())
