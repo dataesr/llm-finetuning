c94f232e0bcf043adcc8f72d9c1b64698016af29
victor barbier <victor.barbier@recherche.gouv.fr>
2025-04-04 15:52:56 +0200

    feat(llm): add redis

--- app/llm/route.py
+++ app/llm/route.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
-from .service import fine_tune, push_model
+import redis
+from rq import Connection, Queue
+from fastapi import APIRouter, Request
+from .service import fine_tune
+from app.logging import get_logger
+
+REDIS_URL = "redis://redis:6379/0"
 
 router = APIRouter()
 
+logger = get_logger(__name__)
 
 @router.get("/test")
 def test():
@@ -11,9 +17,17 @@ def test():
 
 
 @router.post("/ft")
-def run_ft(args: dict):
+def run_ft(request: Request):
+    args = request.json()
     model_name = args.get("model_name")
     dataset_name = args.get("dataset_name")
     output_model_name = args.get("output_model_name")
     huggingface_hub = args.get("huggingface_hub")
-    fine_tune(model_name, dataset_name, output_model_name, huggingface_hub)
+    # assert(args.get('PUBLIC_API_PASSWORD') == PUBLIC_API_PASSWORD)
+    with Connection(redis.from_url(REDIS_URL)):
+        q = Queue("bso-publications", default_timeout=216000)
+        logger.debug("Starting task fine_tune")
+        logger.debug(args)
+        task = q.enqueue(fine_tune, model_name, dataset_name, output_model_name, huggingface_hub)
+    response = {"status": "success", "data": {"task_id": task.get_id()}}
+    return response
