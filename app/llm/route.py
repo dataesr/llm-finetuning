07b01e82ab81318809c95da8e2f3d75a9ae06891
victor barbier <victor.barbier@recherche.gouv.fr>
2025-04-08 12:07:08 +0200

    redis queue

--- app/llm/route.py
+++ app/llm/route.py
@@ -25,7 +25,7 @@ def run_ft(request: Request):
     huggingface_hub = args.get("huggingface_hub")
     # assert(args.get('PUBLIC_API_PASSWORD') == PUBLIC_API_PASSWORD)
     with Connection(redis.from_url(REDIS_URL)):
-        q = Queue("bso-publications", default_timeout=216000)
+        q = Queue("llm", default_timeout=216000)
         logger.debug("Starting task fine_tune")
         logger.debug(args)
         task = q.enqueue(fine_tune, model_name, dataset_name, output_model_name, huggingface_hub)
