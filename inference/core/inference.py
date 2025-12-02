278b4f4d03a3320f467a6fbb1100135101be0188
victor barbier <victor.barbier@recherche.gouv.fr>
2025-12-02 09:57:35 +0100

    inference

--- inference/core/inference.py
+++ inference/core/inference.py
@@ -1,16 +1,42 @@
 import os
-import argparse
 import pandas as pd
-from mlflow import MlflowClient
-from typing import List, Dict, Any, Optional
-from core.generate import load_engine, generate
+from core.generate import generate
 from core.prompts import get_prompts
-from core.completions import merge_and_save
 from shared.mlflow import mlflow_start, mlflow_end, mlflow_set_tags, mlflow_active_model, mlflow_log_artifact
 from shared.dataset import get_dataset
+from shared.utils import timestamp
 from shared.logger import get_logger
 
 logger = get_logger(__name__)
+
+FOLDER = "completions"
+
+
+def merge_and_save(data: pd.DataFrame, completions: list[str], path: str = None):
+    output_col = os.getenv("OUTPUT_COLUMN", "inference")
+    if output_col in data.columns:
+        logger.warning(f"Existing column '{output_col}' will be overridden by generated completions!")
+
+    # Check completions
+    if not isinstance(completions, list):
+        raise TypeError(f"Generated completions must be a list, got {type(completions)}")
+
+    if len(completions) != len(data):
+        logger.error(f"Generated {len(completions)} completions from {len(data)} texts, only completions will be saved")
+        output = pd.DataFrame.from_dict({output_col: completions})
+    else:
+        logger.info(f"✅ Generated {len(completions)}")
+        output = data.copy()
+        output[output_col] = pd.Series(completions)
+
+    # Save to JSON
+    output_path = path or f"completions_{timestamp()}.json"
+    output_path = os.path.join(FOLDER, output_path)
+    if not output_path.endswith(".json"):
+        output_path += ".json"
+    output.to_json(output_path, orient="records")
+
+    return output_path
 
 
 def inference(model_name: str, dataset_name: str, dataset_split: str = "eval", dataset_config: str = None):
